440ccabf1cec24df3b386b87f878e0838c12fb65
Minecraftschurli <minecraftschurli@gmail.com>
2021-03-17 22:23:22 +0100

    fix linting
    Signed-off-by: Minecraftschurli <minecraftschurli@gmail.com>

--- src/app/core/nav/nav.component.spec.ts
+++ src/app/core/nav/nav.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavComponent } from './nav.component';
-import {RouterTestingModule} from "@angular/router/testing";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {LoggingModule} from "../logging/logging.module";
-import {MatDialogModule} from "@angular/material/dialog";
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {LoggingModule} from '../logging/logging.module';
+import {MatDialogModule} from '@angular/material/dialog';
 
 describe('NavComponent', () => {
   let component: NavComponent;

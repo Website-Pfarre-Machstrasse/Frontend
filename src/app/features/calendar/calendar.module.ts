3d090de60ee5436cfca4c19365c44e2cbcc48886
Georg Burkl <gburkl@student.tgm.ac.at>
2021-04-07 00:37:35 +0200

    start implementing calendar with fullcalendar

--- src/app/features/calendar/calendar.module.ts
+++ src/app/features/calendar/calendar.module.ts
@@ -3,13 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarComponent } from './calendar.component';
+import {FullCalendarModule} from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  timeGridPlugin,
+  listPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [CalendarComponent],
   imports: [
     CommonModule,
-    CalendarRoutingModule
+    CalendarRoutingModule,
+    FullCalendarModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class CalendarModule { }

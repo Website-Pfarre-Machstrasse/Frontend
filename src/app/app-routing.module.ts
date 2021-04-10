8aaa0908fe0d9ce5fa288a2dbe5ea6c0938d4273
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-04-10 17:59:06 +0200

    Start Implementierung Galerie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'galerie',
+  loadChildren: () => import('./features/gallery/gallery.module').then(m => m.GalleryModules)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

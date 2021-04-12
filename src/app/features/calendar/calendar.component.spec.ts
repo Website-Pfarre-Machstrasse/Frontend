a1b52a2d6397a211f96c20afbb8240a959eabb36
dneustifter <dneustifter@student.tgm.ac.at>
2021-04-12 14:21:18 +0200

    fix imports

--- src/app/features/calendar/calendar.component.spec.ts
+++ src/app/features/calendar/calendar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalendarComponent } from './calendar.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('CalendarComponent', () => {
   let component: CalendarComponent;
@@ -8,6 +9,7 @@ describe('CalendarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
       declarations: [ CalendarComponent ]
     })
     .compileComponents();

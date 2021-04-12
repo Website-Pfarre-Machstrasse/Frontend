fc5f7bc73953193211b84fc14a69c7d17eec5af0
dneustifter <dneustifter@student.tgm.ac.at>
2021-04-12 14:32:25 +0200

    add router testing module

--- src/app/features/calendar/calendar.component.spec.ts
+++ src/app/features/calendar/calendar.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalendarComponent } from './calendar.component';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {AuthModule} from '../../auth/auth.module';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('CalendarComponent', () => {
   let component: CalendarComponent;
@@ -10,7 +10,7 @@ describe('CalendarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule, AuthModule ],
+      imports: [ HttpClientTestingModule, RouterTestingModule ],
       declarations: [ CalendarComponent ]
     })
     .compileComponents();

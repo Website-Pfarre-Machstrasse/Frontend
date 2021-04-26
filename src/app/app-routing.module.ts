d63c7e1cb137c58c77c2a3b93706342a61e1d738
Minecraftschurli <minecraftschurli@gmail.com>
2021-04-26 15:58:49 +0200

    make default route / not /home
    Signed-off-by: Minecraftschurli <minecraftschurli@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./features/homepage/homepage.module').then(m => m.HomepageModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

66c4bc3b3fd45f1eee8a71a57a36b33d8e581ade
Minecraftschurli <minecraftschurli@gmail.com>
2021-05-05 23:50:01 +0200

    implement save and fix edit button
    Signed-off-by: Minecraftschurli <minecraftschurli@gmail.com>

--- src/app/features/page-renderer/page-renderer.module.ts
+++ src/app/features/page-renderer/page-renderer.module.ts
@@ -5,15 +5,17 @@ import { PageRendererRoutingModule } from './page-renderer-routing.module';
 import { PageRendererComponent } from './page-renderer.component';
 import {SharedModule} from '../../shared/shared.module';
 import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [PageRendererComponent],
     imports: [
-        CommonModule,
-        PageRendererRoutingModule,
-        SharedModule,
-        MatIconModule
+      CommonModule,
+      PageRendererRoutingModule,
+      SharedModule,
+      MatIconModule,
+      MatButtonModule
     ]
 })
 export class PageRendererModule { }

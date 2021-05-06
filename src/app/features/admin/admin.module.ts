09f64f12a5c7e075e11dd0b67244a47d79edfe7b
Minecraftschurli <minecraftschurli@gmail.com>
2021-05-06 12:18:30 +0200

    fix lint errors
    Signed-off-by: Minecraftschurli <minecraftschurli@gmail.com>

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { MatTabsModule } from "@angular/material/tabs";
+import { MatTabsModule } from '@angular/material/tabs';
 import { MenuManagerComponent } from './menu-manager/menu-manager.component';
-import { DragDropModule } from "@angular/cdk/drag-drop";
-import { MatCardModule } from "@angular/material/card";
-import { SharedModule } from "../../shared/shared.module";
-import { MatListModule } from "@angular/material/list";
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatCardModule } from '@angular/material/card';
+import { SharedModule } from '../../shared/shared.module';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({

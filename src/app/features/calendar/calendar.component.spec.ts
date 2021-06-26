3e832d0aefdf6024ed6f58f4a603b62ec939af6a
Georg Burkl <gburkl@student.tgm.ac.at>
2021-06-26 15:53:40 +0200

    fix testing error
    Signed-off-by: Georg Burkl <gburkl@student.tgm.ac.at>

--- src/app/features/calendar/calendar.component.spec.ts
+++ src/app/features/calendar/calendar.component.spec.ts
@@ -4,6 +4,7 @@ import { CalendarComponent } from './calendar.component';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {LoggingModule} from '../../core/logging/logging.module';
+import { MatDialogModule } from '@angular/material/dialog';
 
 describe('CalendarComponent', () => {
   let component: CalendarComponent;
@@ -11,7 +12,7 @@ describe('CalendarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule, RouterTestingModule, LoggingModule.forRoot() ],
+      imports: [ HttpClientTestingModule, RouterTestingModule, LoggingModule.forRoot(), MatDialogModule ],
       declarations: [ CalendarComponent ]
     })
     .compileComponents();
